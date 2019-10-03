61065ea08bd5d0355103d13ba98f9a139f8f3f47
Bilal Ashraf <ba2961289@gmail.com>
2019-10-03 17:22:18 +0500

    Addition of two numbers in start.cpp

---
+++ start.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter the two numbers you want to add: ";
+    cin >> a >> b;
+    sum = (a + b);
+    cout << "The addition of " << a << " and " << b << "are: " << sum;
+
+    return 0;
+}
