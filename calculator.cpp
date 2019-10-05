a5085ac0161cb1701a9b8c9ebba6cddac5135a09
Ehmad Saeed <justehmadsaeed@gmail.com>
2019-10-05 14:51:59 +0500

    calculator.cpp with better calculations

--- calculator.cpp
+++ calculator.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 
 using namespace std;
+float operation(float num1, float num2, char oper);
 int main()
 {
-    int n1 , n2 , result; 
-    char oper;
-    cout<<"The program is calculator of basic interger expression ";
-    cout<<"\nEnter 1st number : ";
-    cin>>n1;
-    cout<<"Enter 2nd number : ";
-    cin>>n2;
-    cout<<"Enter operator : ";
-    cin>>oper;
-    result = operation (n1,n2,oper);
+	float n1, n2, result;
+	char oper;
+	cout << "The program is calculator of basic calculations ";
+	cout << "\nEnter 1st Operand : ";
+	cin >> n1;
+	cout << "Enter 2nd Operand : ";
+	cin >> n2;
+	cout << "Enter operator : ";
+	cin >> oper;
+	result = operation(n1, n2, oper);
 
-    cout<<"This result of integer expression is : "<<result;
-    system("pause");
-    return 0;
+	cout << "This result of integer expression is : " << result << endl;
+	system("pause");
+	return 0;
 }
 
-
-int operation(int num1, int num2, char oper)
+float operation(float num1, float num2, char oper)
 {
-    if (oper == '+')
-        return num1 + num2;
-    else if (oper == '*')
-        return num1 * num2;
-    else if (oper == '-')
-        if (num1 > num2)
-            return num1 - num2;
-        else
-            return num2 - num1;
-    else if (oper == '/')
-        return num1 / num2;
-    else
-        return num1 % num2;
-          
+	while (true)
+	{
+		switch (oper)
+		{
+		case '+':
+			return num1 + num2;
+			break;
+		case '-':
+			return num1 - num2;
+			break;
+		case '*':
+			return num1 * num2;
+			break;
+		case '/':
+			return num1 / num2;
+			break;
+		default:
+			cout << "!!wrong operator!!\nSelect one from the following (+,-,*,/)";
+			cin >> oper;
+			break;
+		}
+	}
 }
