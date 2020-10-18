a5d962f1f9fd866460e2f03540f627e867fe4d59
salman2481 <55204448+salman2481@users.noreply.github.com>
2020-10-18 12:24:14 +0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -46,4 +46,24 @@ int main()
 	return 0;
 }
 #pragma once
+// yar meri b mege kr lain
 
+// program to find the number of elements 
+#include <iostream.>
+using namespace std;
+#define max 10
+int main()
+{
+	int number [max]={1,2,3,4,5,6,7,8,9,10};
+	int index;
+//	cout.set(ios::right);
+	for(index=0;index<max;index++)
+	{
+		cout<<endl<<"the elements of array element are.."<<endl;
+		cout.width(10);
+		cout <<(index +1)<<"is";
+		cout<<number[index];	
+	}
+		cout <<endl<<endl;
+	return 0;
+}
