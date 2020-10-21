053100d23ef0cda89877810d9bf3d8b78829270c
salman2481 <55204448+salman2481@users.noreply.github.com>
2020-10-21 14:47:53 +0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -67,3 +67,50 @@ int main()
 		cout <<endl<<endl;
 	return 0;
 }
+
+
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int i,p, j,temp;
+   cout<<"enter no. of values to enter"<<endl;
+   cin>>p;
+   int a[p] = {};
+   cout <<"Enter input values: \n";
+   for(i = 0; i<p; i++) {
+   	cin>>Array[i];
+      cout <<"\n";
+   }
+
+cout<<endl;
+
+for(i = 0; i<p; i++) {
+
+   for(j = i+1; j<p; j++)
+
+   {
+
+      if(Array[j] < Array[i]) {
+
+         temp = Array[i];
+
+         Array[i] = Array[j];
+
+         Array[j] = temp;
+      }
+   }
+}
+
+cout <<"Sorted Element List ...\n";
+
+for(i = 0; i<p; i++) {
+   cout <<Array[i]<<"\n";
+
+}
+
+return 0;
+
+}
+
